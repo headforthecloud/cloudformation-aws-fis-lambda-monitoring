2f2f199a0d136540fceef8a7514a9a11c8e08b17
headforthecloud <125831551+headforthecloud@users.noreply.github.com>
2025-12-24 14:38:24 +0000

    initial code (#1)
    * initial code
    
    ---------
    
    Co-authored-by: Simon Hanmer <simonhanmer@Mac-mini.localdomain>
    Co-authored-by: Simon Hanmer <simon.hanmer@gmail.com>

---
+++ src/lambda_function.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import boto3
+import os
+import logging
+import json
+
+
+# define a logger using logging library. If LOG_LEVEL is not set, default to INFO.
+# otherwise use value of LOG_LEVEL
+logger = logging.getLogger()
+logger.setLevel(os.getenv('LOG_LEVEL', 'INFO'))
+
+
+# define a lambda_handler function that takes in an event and a context
+def lambda_handler(event, context):
+    logger.info("Hello from Lambda!")
+
+    # create a new boto3 client for the service 'sts'
+    sts = boto3.client('sts')
+
+    # get the id of the AWS account we're running in
+    account_id = sts.get_caller_identity()["Account"]
+    logger.info(f"I'm running in account {account_id}")
+
+    # Return API Gateway compatible response format
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "statusMessage": "All OK",
+            "accountId": account_id
+        })
+    }
